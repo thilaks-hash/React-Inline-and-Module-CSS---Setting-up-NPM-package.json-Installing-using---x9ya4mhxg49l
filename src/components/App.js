39efe47e97a98db4e85a13081806fa521c50ab89
thilaks-hash <thilagavathyr025@gmail.com>
2023-06-22 15:15:22 +0530

    updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,16 @@
-import React from 'react'
-import '../styles/App.css';
-import styles from '../styles/appStyles.module.css';
+import React from "react";
+import "../styles/App.css";
+import styles from "../styles/appStyles.module.css";
 const App = () => {
-const heading ={
-
-
-}
+  const heading = {};
   return (
     <div id="main">
-      <h1 >Success</h1>
-      <h1 id='error' >Error</h1>
+      <h1 className={styles.success}>Success</h1>
+      <h1 id="error" style={{ color: "red" }}>
+        Error
+      </h1>
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
